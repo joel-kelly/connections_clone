af79995ffc03f2fe638f61323f71e7eea6f0d526
joel-kelly <joel.alexander.kelly@gmail.com>
2025-11-06 20:53:29 -0800

    debug localhost issue so i can start testing on a mobile device

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 3069,
     proxy: {
       '/api': {
-        target: 'http://localhost:3070',
+        target: 'http://127.0.0.1:3070',
         changeOrigin: true
       }
     }
